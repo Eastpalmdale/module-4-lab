fecfa153160ab1f00596c18d081742d525f96156
Eastpalmdale <greer.kist@talawanda.org>
2024-02-07 13:52:21 -0500

    1:52pm 2/7/2024

---
+++ src/javavariables.java
@@ -0,0 +1,45 @@
+public class javavariables {
+    public static void main(String[] args) {
+        // integer variables
+        int intOperandB=5;
+        int intOperandA=15;
+        int intSum=0,intProduct=0,intDifference=0,intQuotient=0,intModulo=0;
+        // double variables
+        double doubleOperandA=1.1111412;
+        double doubleOperandB=3.14596586783;
+        double doubleSum, doubleProduct, doubleDifference, doubleQuotient, doubleModulo;
+        // integer math
+        intSum = intOperandA + intOperandB;
+        intProduct = intOperandA * intOperandB;
+        intDifference = intOperandA - intOperandB;
+        intQuotient = intOperandA / intOperandB;
+        intModulo = intOperandA % intOperandB;
+        // double math
+        doubleSum = doubleOperandA + doubleOperandB;
+        doubleProduct = doubleOperandA * doubleOperandB;
+        doubleDifference = doubleOperandA - doubleOperandB;
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        doubleModulo = doubleOperandA % doubleOperandB;
+        // integer outputs
+        System.out.println("Your integer sum is, " + intSum);
+        System.out.println("Your integer product is, " + intProduct);
+        System.out.println("The integer difference is, " + intDifference);
+        System.out.println("The integer quotient is, " + intQuotient);
+        System.out.println("The integer modulo is, " + intModulo);
+        // double outputs
+        System.out.println("Your double sum is, " + doubleSum);
+        System.out.println("Your double product is, " + doubleProduct);
+        System.out.println("The double difference is, " + doubleDifference);
+        System.out.println("The double quotient is, " + doubleQuotient);
+        System.out.println("The double modulo is, " + doubleModulo);
+
+
+
+
+
+
+
+
+
+    }
+}
